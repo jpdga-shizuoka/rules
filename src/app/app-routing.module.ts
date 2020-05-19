e7412f679442f6e87f9875c3b638dc3fc45c1f4a
jpdga-shizuoka <56290481+jpdga-shizuoka@users.noreply.github.com>
2020-05-19 16:27:36 +0900

    update app.compo and app.routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Rule800Component } from './rules/rule800.component';
+import { Rule801Component } from './rules/rule801.component';
+import { Rule80101Component } from './rules/rule80101.component';
+import { Rule80102Component } from './rules/rule80102.component';
+import { Rule80103Component } from './rules/rule80103.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '800', component: Rule800Component},
+  { path: '801', component: Rule801Component},
+  { path: '80101', component: Rule80101Component},
+  { path: '80102', component: Rule80102Component},
+  { path: '80103', component: Rule80103Component},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Rule800Component } from './rules/rule800.component';
+import { Rule801Component } from './rules/rule801.component';
+import { Rule80101Component } from './rules/rule80101.component';
+import { Rule80102Component } from './rules/rule80102.component';
+import { Rule80103Component } from './rules/rule80103.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Rule800Component,
+    Rule801Component,
+    Rule80101Component,
+    Rule80102Component,
+    Rule80103Component,
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
