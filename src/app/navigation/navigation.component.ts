a5e7fbd80c687a561a0c280e21d9731da6f4ec2d
jpdga-shizuoka <56290481+jpdga-shizuoka@users.noreply.github.com>
2020-05-26 15:34:33 +0900

    add prev/next botton to navigate

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,26 +1,101 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { MatSidenav } from '@angular/material/sidenav';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
 import { map, shareReplay } from 'rxjs/operators';
-import { TOC } from '../toc';
+import { Section, TOC, getSection, getNext, getPrev } from '../toc';
+
+const TITLE_PATTERN = /[\d\.]+ (\S+)/;
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
-export class NavigationComponent {
-  toc = TOC;
-
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+export class NavigationComponent implements OnInit, OnDestroy {
+  @ViewChild('drawer') drawer: MatSidenav;
+  readonly toc = TOC;
+  private _section: Section;
+  get section() {return this._section; }
+  set section(section: Section) {
+    if (!section) {
+      return;
+    }
+    this._section = section;
+    const title = section.title.match(TITLE_PATTERN);
+    if (title == null) {
+      return;
+    }
+    this.title = title[1];
+  }
+  title: string;
+  isHandset: boolean;
+  ssHandset: Subscription;
+  readonly isHandset$: Observable<boolean>
+    = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private router: Router,
+  ) {
+    router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+    ).subscribe((event: NavigationEnd) =>
+      this.section = getSection(url2id(event.url)));
+  }
+
+  ngOnInit() {
+    this.ssHandset = this.isHandset$.subscribe(status => this.isHandset = status);
+  }
+
+  ngOnDestroy() {
+    this.ssHandset?.unsubscribe();
+  }
 
   isSubsection(id: string) {
     return /\d{5}/.test(id);
   }
+
+  onClickMenu(id: string) {
+    this.section = getSection(id);
+    if (this.isHandset && this.drawer?.opened) {
+      this.drawer.close();
+    }
+  }
+
+  onClickPrev() {
+    const prev = getPrev(this.section.id);
+    if (!prev) {
+      return;
+    }
+    this.section = prev;
+    this.router.navigate([prev.id]);
+  }
+
+  onClickNext() {
+    const next = getNext(this.section.id);
+    if (!next) {
+      return;
+    }
+    this.section = next;
+    this.router.navigate([next.id]);
+  }
+
+  get isNext() {
+    return this.isHandset && !this.drawer?.opened && !!getNext(this.section?.id);
+  }
+
+  get isPrev() {
+    return this.isHandset && !this.drawer?.opened && !!getPrev(this.section?.id);
+  }
+}
+
+function url2id(url: string) {
+  return url.slice(1,);
 }
--- src/app/toc.ts
+++ src/app/toc.ts
@@ -1,2 +1,29 @@
 import TOC from '../assets/rules/toc.json';
 export { TOC };
+
+export interface Section {
+  id: string;
+  title: string;
+}
+
+export function getSection(id: string): Section {
+  return TOC.find(section => section.id === id);
+}
+
+export function getNext(id: string): Section|undefined {
+  let index = TOC.findIndex(section => section.id === id);
+  if (index == null) {
+    return undefined;
+  }
+  index += 1;
+  return index < TOC.length ? TOC[index] : undefined;
+}
+
+export function getPrev(id: string): Section|undefined {
+  let index = TOC.findIndex(section => section.id === id);
+  if (index == null) {
+    return undefined;
+  }
+  index -= 1;
+  return index < 0 ? undefined : TOC[index];
+}
