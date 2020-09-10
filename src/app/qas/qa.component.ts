50d1479d32dcf13ffdfbd16afd6d645a93ac418e
jpdga-shizuoka <56290481+jpdga-shizuoka@users.noreply.github.com>
2020-09-10 18:21:58 +0900

    add a function to jump-to-pdga for q&a

--- src/app/qas/qa.component.ts
+++ src/app/qas/qa.component.ts
@@ -1,2 +1,28 @@
-export abstract class QaComponent {
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription, Subject, BehaviorSubject } from 'rxjs';
+import { GotoPdgaService } from '../goto-pdga.service';
+
+@Component({
+  selector: 'app-qa',
+  template: '',
+})
+export abstract class QaComponent implements OnInit, OnDestroy {
+  private readonly subscription = new Subscription();
+
+  constructor(private gotoPdgaService: GotoPdgaService) { }
+
+  ngOnInit(): void {
+    this.subscription.add(
+      this.gotoPdgaService.requestJump$.subscribe(() => this.jump2pdga())
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  private jump2pdga() {
+    const url = 'https://www.pdga.com/rules/official-rules-disc-golf/questions-and-answers';
+    window.location.href = url;
+  }
 }
