357361cd0347e356b17859c5e2de916dd55b59c3
jpdga-shizuoka <56290481+jpdga-shizuoka@users.noreply.github.com>
2024-04-23 23:28:24 +0900

    refactoring appendix-e as markdown (#37)
    * Add a markdown file appendix E
    * Add Appendix E component and update routing
    * Refactor styles.css to update app-index selectors
    * add a plugin for span
    * Remove a html file of Appendix E
    * Add markdown-it-span plugin to package.json and package-lock.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -56,11 +56,11 @@ import { Rule812Component } from './rules/rule812.component';
 import { Rule813Component } from './rules/rule813.component';
 import { Rule81301Component } from './rules/rule81301.component';
 import { Rule81302Component } from './rules/rule81302.component';
-import { AppendixEComponent } from './appendix/appendix-e.component';
 import { AppendixAppendixaComponent } from './appendix/appendixappendix-a.component';
 import { AppendixAppendixbComponent } from './appendix/appendixappendix-b.component';
 import { AppendixAppendixcComponent } from './appendix/appendixappendix-c.component';
 import { AppendixAppendixdComponent } from './appendix/appendixappendix-d.component';
+import { AppendixAppendixeComponent } from './appendix/appendixappendix-e.component';
 import { AppendixAppendixfComponent } from './appendix/appendixappendix-f.component';
 import { QaIndexComponent } from './qas/qaindex.component';
 import { QaAppComponent } from './qas/qaapp.component';
@@ -148,11 +148,11 @@ import { PullToJumpComponent } from './pull-to-jump/pull-to-jump.component';
     Rule813Component,
     Rule81301Component,
     Rule81302Component,
-    AppendixEComponent,
     AppendixAppendixaComponent,
     AppendixAppendixbComponent,
     AppendixAppendixcComponent,
     AppendixAppendixdComponent,
+    AppendixAppendixeComponent,
     AppendixAppendixfComponent,
     QaIndexComponent,
     QaAppComponent,
--- src/app/appendix/appendix-e.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-import { AppendixComponent } from './appendix.component';
-@Component({
-  selector: 'app-appendix-e',
-  templateUrl: './appendix-e.html'
-})
-export class AppendixEComponent extends AppendixComponent { }
