8a760a74d7f8b61047356aaecce11c2ec8137155
jpdga-shizuoka <56290481+jpdga-shizuoka@users.noreply.github.com>
2024-11-28 17:45:09 +0900

    pandocフィルター雛形

--- pandoc/filters/sub-url.py
+++ pandoc/filters/sub-url.py
@@ -1,20 +1,72 @@
 #!/usr/bin/env python3
 #
 # `ordg/`は、`https://jpdga-shizuoka.github.io/rules/`に
-# `dgj/`は、`https://jpdga-shizuoka.github.io/ssa-round-ratings/libraries/`に
 # 置き換えるフィルター
 # 
 import panflute as pf
-
+#
+# フラグメント識別子をHTML形式からPANDOC形式に変換
+#
+LINK_MAP = {
+    '800': 'ゲームの説明',
+    '801': '規則の適用',
+    '80101': '公平性',
+    '80102': '執行',
+    '80103': '抗議',
+    '802': 'ディスクを投げる',
+    '80201': 'スロー',
+    '80202': 'プレーの順番',
+    '80203': '時間超過',
+    '80204': 'ティーの開始',
+    '80205': 'ライ',
+    '80206': 'ライをマークする',
+    '80207': 'スタンス',
+    '803': '障害物と救済',
+    '80301': '動かせる障害物',
+    '80302': '障害物からの救済',
+    '80303': 'コースの損壊',
+    '804': '経路の規制',
+    '80401': 'マンダトリーの経路',
+    '805': '位置の規制',
+    '80501': '位置の確定',
+    '80502': '2mより上空にあるディスク',
+    '80503': 'ディスクの紛失',
+    '806': '区域の規制',
+    '80601': 'パッティングエリア',
+    '80602': 'アウト・オブ・バウンズ',
+    '80603': 'カジュアルエリア',
+    '80604': '救済区域',
+    '80605': 'ハザード',
+    '807': 'ホールの終了',
+    '808': 'スコアの記録',
+    '809': 'その他のスロー',
+    '80901': 'スローの放棄',
+    '80902': '暫定スロー',
+    '80903': '練習スロー',
+    '810': '干渉',
+    '811': 'ミスプレー',
+    '812': '礼儀',
+    '813': '装備',
+    '81301': '不正なディスク',
+    '81302': '不正な用具',
+    'appendix-a': '附則a-マッチプレー',
+    'appendix-b': '附則b-ダブルスとチームプレー',
+    'appendix-c': '附則c-資料',
+    'appendix-d': '附則d-単位の換算',
+    'appendix-e': '附則e-索引',
+    'appendix-f': '附則f-ディスクゴルフの適応規則',
+}
+#
+#
+#
 def replace_urls(elem, doc):
     if isinstance(elem, pf.Link):
-        # 元のURLを取得
         url = elem.url
-        # URLの置き換え
-        if url.startswith("ordg/"):
-            elem.url = url.replace("ordg/", "https://jpdga-shizuoka.github.io/rules/")
-        elif url.startswith("dgj/"):
-            elem.url = url.replace("dgj/", "https://jpdga-shizuoka.github.io/ssa-round-ratings/libraries/")
+        if not url.startswith("http"):
+            fragment = LINK_MAP[url]
+            pf.debug(f'{fragment =}')
+            if fragment:
+                elem.url = f'#{fragment}'
     return elem
 
 def main(doc=None):
